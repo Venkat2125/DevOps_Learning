dff17af426d732c163d5b0e89967c5c94056b368
Venkat2125 <venkatsavi2125@gmail.com>
2026-03-24 13:40:05 +0530

    changes

--- shell-practice/variable.sh
+++ shell-practice/variable.sh
@@ -4,3 +4,15 @@ person1="shivanna"
 person2="lakshmi"
 echo $person1 : "Hello $person2"
 echo $person2 : "Hello $person1"
+#task1
+name="venkat"
+city="hyderaba"
+echo "my name is $name"
+echo "I live in Hyderabad"
+#task 2:
+#sum print
+a=10
+b=20
+c=$((a+b))
+echo "sum is $c"
+#task3:
