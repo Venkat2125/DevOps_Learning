99a1a66dece79f2f99c0c471f710c8004c9ba488
Venkat2125 <venkatsavi2125@gmail.com>
2026-03-24 23:31:36 +0530

    changes

--- shell-practice/variable1.sh
+++ shell-practice/variable1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-date=$(date)
-echo "time stamp $date"
+#date=$(date)
+#echo "time stamp $date"
+start_time=$(date +%s)
+sleep 10
+End_tme=$(date +%s)
+Total_time=$(($End_tme-$start_time))
+echo "script executed in: $()"
