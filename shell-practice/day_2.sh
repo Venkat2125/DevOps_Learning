6769d9e3bc4f7dac8fdcbf3d212f61377f13f7ef
Venkat2125 <venkatsavi2125@gmail.com>
2026-03-24 18:25:00 +0530

    day2 prctice

---
+++ shell-practice/day_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "venkat : HEllo shivanna, welcome to AP"
+echo "shivanna : Thank you,Nice to met venkat"
+echo "venkat : Nice tomeet shivanna"
+echo "shivanna : venkat where to you go"
